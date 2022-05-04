7ab447d4a87a2c7280cce1dbd88a6f549148a8de
TeaEditor <48626361+TeaEditor@users.noreply.github.com>
2022-05-04 22:48:07 +0100

    Version 1.0.1
    Code update, command implementations and fixes

--- commands/rise.js
+++ commands/rise.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('Rise')
+		.setName('rise')
 		.setDescription('Replies with info about Rise!'),
 	async execute(interaction) {
 		await interaction.reply("The wondeful Rise is who you've got to thank for slash commands being added!");
