06579d3d02e4c9a64aeb91ed6b40a8cda2389f7f
TeaEditor <48626361+TeaEditor@users.noreply.github.com>
2022-05-06 18:47:38 +0100

    Event handling
    Added event-handling, slash commands currently not working, will fix.

--- events/ready.js
+++ events/ready.js
@@ -5,11 +5,3 @@ module.exports = {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
-}
-
