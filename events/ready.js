7cd2afee584ebb68b970d6dfba0438b57a2b9e60
TeaEditor <48626361+TeaEditor@users.noreply.github.com>
2022-05-06 17:21:20 +0100

    V1.0.1
    Added event handling, ready.js and create.js

---
+++ events/ready.js
@@ -0,0 +1,15 @@
+module.exports = {
+	name: 'ready',
+	once: true,
+	execute(client) {
+		console.log(`Ready! Logged in as ${client.user.tag}`);
+	},
+};
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
